c1dd6214cdbe7b7f733e2882d3986b83304e16cc
JessAcke <juarezyisus554@gmail.com>
2025-06-11 13:32:09 -0600

    Filtrar Datos, calcular y agrupar

--- excel.py
+++ excel.py
@@ -15,3 +15,18 @@ class ProcesadorPiezas:
         self.df["fecha"] = pd.to_datetime(self.df["fecha"], errors="coerce").dt.tz_localize(None)
         self.df["M5_C"] = pd.to_numeric(self.df["M5_C"], errors="coerce").fillna(0).astype(int)
         self.df.sort_values(by="fecha", ascending=True, inplace=True)
+
+    def filtrar_intervalo(self):
+        self.df = self.df[(self.df["fecha"] >= self.fecha_inicio) & (self.df["fecha"] <= self.fecha_fin)]
+
+    def calcular_piezas(self):
+        self.df["Piezas_Hechas"] = self.df["M5_C"].diff().fillna(0).astype(int).abs()
+
+    def agrupar_por_intervalos(self):
+        self.df["Fecha Inicio"] = self.df["fecha"].apply(
+            lambda x: self.fecha_inicio + timedelta(minutes=((x.minute // 5) * 5))
+        )
+        self.df["Fecha Fin"] = self.df["Fecha Inicio"] + timedelta(minutes=4)
+        self.resultado = self.df.groupby(["Fecha Inicio", "Fecha Fin"])["Piezas_Hechas"].sum().reset_index()
+
+
---
+++ jesus_procesamiento.py
@@ -0,0 +1,5 @@
+
+from raul_procesamiento import procesador  # Continuación del objeto creado
+
+procesador.calcular_piezas()
+procesador.agrupar_por_intervalos()
